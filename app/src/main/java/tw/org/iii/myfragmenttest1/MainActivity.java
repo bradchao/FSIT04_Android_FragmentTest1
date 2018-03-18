c148541d99d377613d6f6883065529559d00b84e
Brad <brad@brad.tw>
2018-03-18 12:03:06 +0800

    v2

--- app/src/main/java/tw/org/iii/myfragmenttest1/MainActivity.java
+++ app/src/main/java/tw/org/iii/myfragmenttest1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private F2 f2;
     private FragmentManager fmgr;
     private FragmentTransaction tran;
+    private boolean isF1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +24,16 @@ public class MainActivity extends AppCompatActivity {
         tran = fmgr.beginTransaction();
         tran.add(R.id.container, f1);
         tran.commit();
+
+        isF1 = true;
     }
 
     public void change(View view) {
+        isF1 = !isF1;
+
         tran = fmgr.beginTransaction();
-        tran.replace(R.id.container, f2);
+        tran.replace(R.id.container, isF1?f1:f2);
+        tran.addToBackStack(null);
         tran.commit();
     }
 
